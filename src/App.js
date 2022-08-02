4b31851c471671b7f8537c53325f5247b8588484
zenfall <moulayehassan20@gmail.com>
2022-08-02 20:37:35 +0000

    new push build jenkins

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React App New
+          React App 
         </a>
       </header>
     </div>
